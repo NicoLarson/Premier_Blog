65da09865932de39f77e20163c80410ccdf4d82f
Nicolas Yang <nicolas.yang.1@gmail.com>
2021-04-06 23:54:25 +0200

    Add DTO + Update src

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,16 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Accounts;
+use App\Entity\Account;
 use App\Form\SignInType;
 
+use App\DTO\AccountCreate;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
@@ -19,17 +20,18 @@ class SecurityController extends AbstractController
     #[Route('/signin', name: 'signin')]
     public function signin(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
     {
-        $account = new Accounts();
-        $form = $this->createForm(SignInType::class, $account);
+        $form = $this->createForm(SignInType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @var AccountCreate $accountDTO */
+            $accountDTO = $form->getData();
+            $account = new Account($accountDTO->email, $accountDTO->username, $accountDTO->password);
             $hash = $encoder->encodePassword($account, $account->getPassword());
             $account->setPassword($hash);
             $manager->persist($account);
             $manager->flush();
             return $this->redirectToRoute('app_login');
-
         }
 
         return $this->render('security/signin.html.twig', [
@@ -42,9 +44,6 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -61,5 +60,4 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
-
 }
